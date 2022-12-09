d6519a96500205304746cadcefff2669c8336756
NatKwok <natkwok902@gmail.com>
2022-12-09 04:19:54 -0500

    Update AdminDash.js

--- frontend/src/components/AdminDash/AdminDash.js
+++ frontend/src/components/AdminDash/AdminDash.js
@@ -1,9 +1,17 @@
 import './App.css';
 import UserTable from './components/UserTable';
+import { Button } from '@mui/material';
 
 function AdminDash() {
   return (
-    <UserTable />
+    <div>
+            <div className="nav">
+        <Button variant="outlined" className="button">Home</Button>
+        <Button variant="outlined" className="button">Logout</Button>
+      </div>
+      <UserTable />
+    </div>
+    
   )
 }
 
